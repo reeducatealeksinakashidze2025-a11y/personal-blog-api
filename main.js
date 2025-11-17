b1fc329b33a4b46a90e68e242eff2eb947239784
reeducatealeksinakashidze2025-a11y <reeducate.aleksinakashidze2025@gmail.com>
2025-11-17 15:28:39 +0400

    edit

--- main.js
+++ main.js
@@ -11,12 +11,8 @@ const app = express()
 
 
 app.use(express.json())
-// app.use(cors());
-app.use(cors({
-          origin:  '*',
-          methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-          allowedHeaders: ['Content-Type', 'Authorization'],
-        }));
+app.use(cors());
+
 app.use(logger)
 app.use('/users', userRouter)
 app.use('/auth', authRouter)
